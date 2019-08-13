629a36678698b45a6338458296c2143ed7b42ccc
kevy7 <kvnyang503@gmail.com>
2019-08-12 21:31:59 -0700

    created first route in index.js file

--- index.js
+++ index.js
@@ -1,4 +1,44 @@
 require('dotenv').config(); //In order to gain access to our .env file
 //process.env.YELP_API_KEY
+const express = require("express");
+const app = express();
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+let port = process.env.PORT || 5000;
+//const pg = require("pg");
+
+
 
 //made a comment here
+
+app.get("/", function(req, res){
+
+    //How to run a query using postgresSQL
+    /*
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){
+        if(err) {
+            console.log(err);
+        }
+        else {
+            client.query('SELECT * FROM YOURTABLE', function(err, results){
+                if(err){
+                    console.log(err);
+                }
+                else {
+                    console.log(results);
+                    done(); //This is always going to be placed last
+                }
+            })
+        }
+    })
+    */
+})
+
+
+app.listen(port, function(){
+    console.log("Your app is running on port " + port);
+})
