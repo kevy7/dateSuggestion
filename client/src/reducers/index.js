ead24635dee1521bc268c6ce674e939bff00256e
kevy7 <kvnyang503@gmail.com>
2019-09-02 22:01:59 -0700

    Setting up the store.js file

---
+++ client/src/reducers/index.js
@@ -0,0 +1,11 @@
+/*
+
+Main index file for our reducers
+
+*/
+
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    //reducer info will be placed in here
+});
--- client/src/store.js
+++ client/src/store.js
@@ -5,4 +5,20 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
 //need to ccreate reducers as well and import it
-//import reducers from './reducers';
+import reducers from './reducers';
+
+const initialState = {}; //Creating our initial state
+const middleware = [thunk]; //Setting up middleware to make api calls
+
+export const store = createStore(
+    initialState,
+    applyMiddleware(...middleware)
+
+    //This may be needed to view redux state in Chrome
+    /*
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+    */
+);
