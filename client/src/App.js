d8a3f31e6d0769b652d5ff820f3764eeaa74d95e
kevy7 <kvnyang503@gmail.com>
2019-09-02 21:54:48 -0700

    setting our app to work with redux. Partially completed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Switch, Route, withRouter } from 'react-router-dom';
+
 
 function App() {
   return (
---
+++ client/src/store.js
@@ -0,0 +1,8 @@
+/*
+This is needed for setting up our app with redux
+*/
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+
+//need to ccreate reducers as well and import it
+//import reducers from './reducers';
