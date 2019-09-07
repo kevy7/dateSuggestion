1ad3b10c13190cf0cc92c1e6f50c90fdf6de0246
kevy7 <kvnyang503@gmail.com>
2019-09-07 11:30:32 -0700

    modified inputDish.js file so that it can store user's input

--- client/src/components/InputDish/InputDish.js
+++ client/src/components/InputDish/InputDish.js
@@ -4,7 +4,43 @@ import { withRouter } from 'react-router-dom';
 
 import styles from './InputDish.module.css';
 
+/*
+
+Review this for creating an input list
+
+<input list="browsers">
+
+<datalist id="browsers">
+  <option value="Internet Explorer">
+  <option value="Firefox">
+  <option value="Google Chrome">
+  <option value="Opera">
+  <option value="Safari">
+</datalist>
+
+*/
+
+/*
+dish
+dishDescription
+
+We also need to pass in the user
+*/
+
+
+
 class InputDish extends Component {
+
+    state = {
+        dish: "",
+        dishDescription: ""
+    }
+
+    handleInputChanges = async (e) => {
+        await this.setState({
+            [e.target.name]: e.target.value
+        });
+    }
 
     render(){
         return (
@@ -13,11 +49,25 @@ class InputDish extends Component {
                     <form>
                         <div className="form-group">
                             <label for="exampleFormControlInput1">Dish Name</label>
-                            <input type="email" className="form-control" id="exampleFormControlInput1" />
+                            <input 
+                                type="email" 
+                                className="form-control" 
+                                id="exampleFormControlInput1" 
+                                name="dish"
+                                onChange={this.handleInputChanges}
+                                value={this.state.dish}
+                            />
                         </div>
                         <div className="form-group">
                             <label for="exampleFormControlTextarea1">Dish Description</label>
-                            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                            <textarea 
+                                className="form-control" 
+                                id="exampleFormControlTextarea1" 
+                                rows="3"
+                                name="dishDescription"
+                                onChange={this.handleInputChanges}
+                                value={this.state.dishDescription}
+                            ></textarea>
                         </div>
                         <button type="button" class="btn btn-primary">Submit</button>
                     </form>
@@ -27,4 +77,12 @@ class InputDish extends Component {
     }
 }
 
-export default InputDish;
+const mapStateToProps = (state) => {
+    return {
+        loginUser: state.loginUser
+    }
+}
+
+export default withRouter(connect(mapStateToProps, {
+    //Actions are passed in here
+})(InputDish));
