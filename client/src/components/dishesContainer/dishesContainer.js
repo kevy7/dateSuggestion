3d88422409a3f5a599e73f17d32c67c996230f8c
kevy7 <kvnyang503@gmail.com>
2019-09-09 20:35:20 -0700

    created the dishesContainer

---
+++ client/src/components/dishesContainer/dishesContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+class DishesContainer extends Component {
+
+    componentWillMount = () => {
+
+    }
+
+    render(){
+        return (
+            <div className = "dishesContainer">
+
+            </div>
+        )
+    }
+}
+
+export default DishesContainer;
