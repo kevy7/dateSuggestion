e1e3399e521f807e3f3c1e81dbada745005f42d3
kevy7 <kvnyang503@gmail.com>
2019-09-19 22:02:02 -0700

    added styling to navbar

--- client/src/components/dishesContainer/DishNavbar.js
+++ client/src/components/dishesContainer/DishNavbar.js
@@ -2,12 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
+import styles from './DishNavbar.module.css';
+
 class DishNavBar extends Component {
 
     render(){
         return(
             <div className="DishNavbar">
-                <p>this should be a navbar</p>
+                <ul className="nav justify-content-center">
+                    <li className="nav-item" className={styles.navItem}>
+                        <div className="input-group mb-3">
+                            {/* <div class="input-group-prepend">
+                                <button class="btn btn-outline-secondary" type="button">Button</button>
+                            </div> */}
+                            <input type="text" className="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1" />
+                        </div>
+                    </li>
+                </ul>
             </div>
         )
     }
