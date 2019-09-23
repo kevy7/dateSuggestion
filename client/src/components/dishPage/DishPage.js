3750ed2557f937b555423113c42bf0d15762b4cc
kevy7 <kvnyang503@gmail.com>
2019-09-22 19:22:47 -0700

    added code to the DishPage component

---
+++ client/src/components/dishPage/DishPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import styles from './DishPage.module.css';
+
+class DishPage extends Component {
+    render(){
+        return(
+            <div className="DishPage">
+                
+            </div>
+        )
+    }
+}
+
+export default DishPage;
