4d492385d5bff77357e8aef187583525a66019f6
kevy7 <kvnyang503@gmail.com>
2019-10-12 15:11:31 -0700

    fixing the RecipeComponent

--- client/src/components/dishPage/subComponents/RecipeContainer/RecipeListContainer/RecipeComponents/RecipeComponents.js
+++ client/src/components/dishPage/subComponents/RecipeContainer/RecipeListContainer/RecipeComponents/RecipeComponents.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter, Link } from 'react-router-dom';
 
+import { selectRecipe } from '../../../../../../actions';
 import styles from './RecipeComponents.module.css';
 
 //set background image to the passed in image via props
@@ -9,27 +10,25 @@ import styles from './RecipeComponents.module.css';
 class RecipeComponents extends Component {
     //"/dishes/:id/recipes/:recipe"
 
+    ClickMe = () => {
+        alert("You clicked on me!!");
+    }
+
     render(){
 
         /* const imageSrc = "https://www.edamam.com/web-img/8c3/8c32f359fc50fd6b86cff8d6511bfb46.jpg"; //this should be retrieved via a prop
         const imageStyle = {
             backgroundImage: `url(${imageSrc})`
         } */
-
-        const dishID = this.props.match.params.id;
-        const recipe = this.props.recipeName;
-
-        const url = "/dishes/" + dishID + "/recipes/" + recipe;
-
-        console.log(url);
+        const url = "/dishes/" + this.props.match.params.id + "/recipes/" + this.props.recipeName;
 
 
         return (
             <div className={styles.recipeComponent} /* style={imageStyle} */>
-                <Link to={url}>
+                <Link to={url} onClick={this.ClickMe}>
                     <img src={this.props.recipeImage} className={styles.recipeImage} />
                 </Link>
-                <p className={styles.recipeTitle}>{this.props.recipeName}</p>
+                <p className={styles.recipeTitle} onClick={this.ClickMe}>{this.props.recipeName}</p>
             </div>
         )
     }
