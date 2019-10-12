5ce9dae373791ca8c6371b11ce74bc01a55606dc
kevy7 <kvnyang503@gmail.com>
2019-10-12 14:37:19 -0700

    links now work in RecipeComponents

--- client/src/components/dishPage/subComponents/RecipeContainer/RecipeListContainer/RecipeComponents/RecipeComponents.js
+++ client/src/components/dishPage/subComponents/RecipeContainer/RecipeListContainer/RecipeComponents/RecipeComponents.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 import styles from './RecipeComponents.module.css';
 
@@ -9,13 +9,6 @@ import styles from './RecipeComponents.module.css';
 class RecipeComponents extends Component {
     //"/dishes/:id/recipes/:recipe"
 
-    /* const dishID = this.props.match.params.id;
-    const recipe = this.props.match.params.recipe;
-
-    const url = "/dishes/" + dishID + "/recipes/" + recipe;
-
-    console.log(url); */
-
     render(){
 
         /* const imageSrc = "https://www.edamam.com/web-img/8c3/8c32f359fc50fd6b86cff8d6511bfb46.jpg"; //this should be retrieved via a prop
@@ -23,11 +16,19 @@ class RecipeComponents extends Component {
             backgroundImage: `url(${imageSrc})`
         } */
 
-        
+        const dishID = this.props.match.params.id;
+        const recipe = this.props.recipeName;
+
+        const url = "/dishes/" + dishID + "/recipes/" + recipe;
+
+        console.log(url);
+
 
         return (
             <div className={styles.recipeComponent} /* style={imageStyle} */>
-                <img src={this.props.recipeImage} className={styles.recipeImage} />
+                <Link to={url}>
+                    <img src={this.props.recipeImage} className={styles.recipeImage} />
+                </Link>
                 <p className={styles.recipeTitle}>{this.props.recipeName}</p>
             </div>
         )
