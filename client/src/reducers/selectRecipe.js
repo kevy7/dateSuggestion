bdc702949917ebbd01f0f06fd36733b337dc5f2a
kevy7 <kvnyang503@gmail.com>
2019-10-12 11:54:26 -0700

    edited reducer

--- client/src/reducers/selectRecipe.js
+++ client/src/reducers/selectRecipe.js
@@ -5,3 +5,25 @@ const initialState = {
     loading: false
 }
 
+const selectedRecipe = (state=initialState, action) => {
+    if(action.type === LOAD_COMPONENT){
+        return {
+            ...state,
+            loading: true
+        }
+    }
+    else if (action.type === SELECT_RECIPE){
+        return {
+            ...state,
+            loading: false,
+            selectedRecipe: action.payload
+        }
+    }
+    else {
+        return {
+            ...state
+        }
+    }
+}
+
+export default selectedRecipe;
