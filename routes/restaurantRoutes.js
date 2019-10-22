6989a3d65fd2beae50cbddeb33d4d6aa3e3476ac
kevy7 <kvnyang503@gmail.com>
2019-10-21 20:12:06 -0700

    updated restaurant routes

--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -15,5 +15,39 @@ const pool = new Pool({
 //Route for getting a list of restaurants
 router.get('/api/dishes/restaurants', (req, res) => {
 
+    //research the yelp restuarants api
+
+    /*
+        var config = {
+            headers: {'Authorization': "bearer " + token}
+        };
+
+        var bodyParameters = {
+        key: "value"
+        }
+
+        Axios.post( 
+            'http://localhost:8000/api/v1/get_token_payloads',
+            bodyParameters,
+            config
+        ).then((response) => {
+            console.log(response)
+        }).catch((error) => {
+            console.log(error)
+        });
+    */
+
+    let config = {
+        headers: {'Authorization': 'bearer ' + process.env.YELP_API_KEY}
+    }
+
+    //How to pass in parameters using axios
+    const parameter = {
+        params: {
+            location: 'Portland',
+            term: 'pho'
+        }
+    }
+
     
 })
