f6253d53b162ceb0c7615eb58057449fa8e710fa
kevy7 <kvnyang503@gmail.com>
2019-10-23 21:12:50 -0700

    updated reducer

--- client/src/reducers/getUserRestaurants.js
+++ client/src/reducers/getUserRestaurants.js
@@ -1,5 +1,12 @@
 import { SELECT_RESTAURANT, LOAD_COMPONENT } from '../actions/types';
 
 const initialState = {
-
+    restaurants: {},
+    loading: false
 }
+
+const getUserRestaReducer = (state=initialState, action) => {
+    
+}
+
+export default getUserRestaReducer;
