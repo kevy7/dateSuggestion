794e498636ef8ffc921955cbf541333c579f742f
kevy7 <kvnyang503@gmail.com>
2019-10-26 15:52:47 -0700

    currently in the process of retreiving users longitude and latitude

--- client/src/components/dishPage/subComponents/RestaurantsContainer/RestaurantsListContainer/RestaurantsListContainer.js
+++ client/src/components/dishPage/subComponents/RestaurantsContainer/RestaurantsListContainer/RestaurantsListContainer.js
@@ -8,12 +8,33 @@ class RestaurantsListContainer extends Component {
     //Container will be placed in here
     componentDidMount = () => {
 
+        const success = () => {
+
+        }
+
+        const error = () => {
+
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error);
+
+        
+        const userData = {
+            userDish: this.props.match.params.id, //This will give us the selected dish name from the url
+            location: '', //Find out how to get the user's current location
+            /* //Should start thinking about using these two
+            latitude: '',
+            longitude: ''
+            */
+        }
+
+        console.log(this.props.match.params.id);
     }
 
     render(){
         return (
             <div className="RestListContainer">
-                <h2>This is the RestListContainer</h2>
+                <h2>This is the RestaListContainer</h2>
             </div>
         )
     }
@@ -21,7 +42,7 @@ class RestaurantsListContainer extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        restaurants: state.restaurants //will return us a list of restaurants via our api route
+        restaurants: state.restaurants, //will return us a list of restaurants via our api route
     }
 }
 
