1abb68880e6309cefbff68205127a504b3bd7d9a
kevy7 <kvnyang503@gmail.com>
2019-11-03 18:59:16 -0800

    updated DishComponent to be a class-based component rather than functional

--- client/src/components/dishesContainer/dishComponent.js
+++ client/src/components/dishesContainer/dishComponent.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import styles from './DishComponent.module.css';
 import { selectDish } from '../../actions/index';
 
-const DishComponent = (state) => {
-    const style = {
-        height: '100%'
-    }
-    const url = "/dishes/" + state.dish_name + "/Recipes";
-    return (
-        <div className={styles.dishComponent}>
-            <div className="card" style={style} /*className={styles.card}*/ >
-                    {/* <img src="..." className="card-img-top" alt="..." /> */}
-                    <div className="card-body" className={styles.card}>
-                        <h5 className="card-title">{state.dish_name}</h5>
-                        <p className="card-text">{state.dish_description}</p>
-                        <Link to={url} className="btn btn-primary">View Recipes and Places</Link>
-                    </div>
+class DishComponent extends Component {
+    render(){
+        const style = {
+            height: '100%'
+        }
+        const url = "/dishes/" + this.props.dish_name + "/Recipes";
+        return (
+            <div className={styles.dishComponent}>
+                <div className="card" style={style} /*className={styles.card}*/ >
+                        {/* <img src="..." className="card-img-top" alt="..." /> */}
+                        <div className="card-body" className={styles.card}>
+                            <h5 className="card-title">{this.props.dish_name}</h5>
+                            <p className="card-text">{this.props.dish_description}</p>
+                            <Link to={url} className="btn btn-primary">View Recipes and Places</Link>
+                        </div>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 //Below, are our two props that is expected to be passed down below
@@ -30,4 +34,12 @@ DishComponent.propTypes = { //propTypes are lowercased when declaring within you
     dish_description: PropTypes.string
 }
 
-export default DishComponent;
+const mapStateToProps = (state) => {
+    return {
+
+    }
+}
+
+export default withRouter(connect(mapStateToProps, {
+    
+})(DishComponent));
