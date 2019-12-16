dc1613b5c16086d85b7e10fa142915ba55f4b486
kevy7 <kvnyang503@gmail.com>
2019-12-15 17:45:40 -0800

    updated component

---
+++ client/src/components/dishPage/subComponents/RestaurantsContainer/RestaurantDescContainer/RestaDescContainer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+//Maybe import a component in here
+
+class RestaDescContainer extends Component {
+
+    render(){
+        return (
+            <div className="RestaDescContainer">
+
+            </div>
+        )
+    }
+}
+
+export default RestaDescContainer;
+
