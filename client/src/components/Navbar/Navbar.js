6dba4615dda95d7d631868eb9b39b68a9d2ee71c
kevy7 <kvnyang503@gmail.com>
2019-12-30 21:38:04 -0800

    imported withRouter from the Navbar component

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
