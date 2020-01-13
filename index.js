6f95b1e6bfb6f7c838c8cd17e159aaf306b0ba78
kevy7 <kvnyang503@gmail.com>
2020-01-12 21:57:41 -0800

    updated package.json and updated backend route

--- index.js
+++ index.js
@@ -95,6 +95,20 @@ app.get("/poop", (req, res) => {
     console.log(req.isAuthenticated());
 })
 
+
+//check to see if we're on heroku
+if(process.env.NODE_ENV === 'production') {
+    //Basically, if we're in production then run the code below
+    app.use(express.static('client/build'));
+
+    //For any routes that gets hit here, we're going to load the react index.html file
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
+
 app.listen(port, function(){
     console.log("Your app is running on port " + port);
 });
